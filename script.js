7567f7f66c4a67d076a9a62ccdf399f9e548c064
Nicoly Mendes <111599909+NyMendes@users.noreply.github.com>
2025-02-27 15:04:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+new window.VLibras.Widget('https://vlibras.gov.br/app');
+// Este script inicializa o widget VLibras. A função 'VLibras.Widget' é chamada com a URL base do app VLibras, configurando o widget no site.
+
+// Adiciona um evento de mudança ao elemento com o id 'color-filter'
+document.getElementById('color-filter').addEventListener('change', function() {
+    // atribui o valor da propriedade value do elemento atual à variável filterClass.
+    const filterClass = this.value; /*váriavel constante*/
+
+    // Seleciona todas as imagens com a classe 'filter-image'
+    const images = document.querySelectorAll('.filter-image');
+
+    // Para cada imagem selecionada... método de array que executa uma função para cada elemento do array.
+    images.forEach(img => {
+        // Remove todas as classes existentes da imagem, exceto 'filter-image'
+        img.className = 'filter-image'; // Reset the class
+
+        // Se a classe selecionada não for 'default' (ou seja, uma opção de filtro foi selecionada)...
+        if (filterClass !== 'default') {
+            // Adiciona a classe selecionada à imagem
+            img.classList.add(filterClass);
+        }
+    });
+});
